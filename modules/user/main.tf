648a6aba21b1478600066b94e3f0193f78adc70d
gandalf the white <gandalf_the_white@orange.fr>
2026-07-25 15:15:38 +0200

    Update

---
+++ modules/user/main.tf
@@ -0,0 +1,118 @@
+
+####################################################################################
+##  RESOURCES
+####################################################################################
+
+resource "proxmox_vm_qemu" "server" {
+  description = "Deploiement VM Debian on Proxmox"
+  name        = var.name
+  target_node = var.target_node
+  clone       = var.clone
+
+  os_type  = "cloud-init"
+  memory   = var.memory
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  agent    = 1
+
+  cpu {
+    type    = "host"
+    cores   = 1
+    sockets = 1
+  }
+
+  tags = "Linux;User"
+
+  cicustom = "user=${var.cloudinit}:snippets/cloudinit.yaml"
+
+  disks {
+    ide {
+      ide3 {
+        cloudinit {
+          storage = var.storage
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size    = var.size
+          storage = var.storage
+        }
+      }
+    }
+  }
+
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = var.bridge
+    tag    = var.vlan
+  }
+
+  ipconfig0  = "ip=${var.prefix}.${var.octet}/24,gw=${var.prefix}.1"
+  nameserver = var.nameserver
+
+  provisioner "remote-exec" {
+    inline = [
+      "cloud-init status --wait",
+      "sudo hostnamectl set-hostname ${var.name}"
+    ]
+
+    connection {
+      host        = "${var.prefix}.${var.octet}"
+      type        = "ssh"
+      user        = var.userctn
+      private_key = file(var.privkeyctn)
+    }
+  }
+}
+
+####################################################################################
+##  ANSIBLE
+####################################################################################
+
+resource "local_file" "inventory" {
+  content = templatefile("${path.module}/manifests/inventory-template.yaml",
+    {
+      ipaddress  = "${var.prefix}.${var.octet}"
+      userctn    = var.userctn
+      privkeyctn = var.privkeyctn
+      name       = var.name
+  })
+  filename        = "./ansible/inventory-user.yaml"
+  file_permission = "0644"
+}
+
+
+resource "local_file" "playbook" {
+  content = templatefile("${path.module}/manifests/playbook-template.yaml",
+    {
+      hostname = var.name
+      nfs      = "${var.prefix}.${var.nfs}"
+      proxy    = var.proxy
+      noproxy  = "${var.prefix}.0/24"
+  })
+  filename        = "./ansible/playbook-user.yaml"
+  file_permission = "0644"
+}
+
+resource "null_resource" "play_ansible" {
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ansible/inventory-user.yaml ansible/playbook-user.yaml"
+  }
+  depends_on = [
+    proxmox_vm_qemu.server,
+    local_file.inventory,
+    local_file.playbook
+  ]
+}
+
+####################################################################################
+##  OUTPUT
+####################################################################################
+
+output "user_server_ip_address" {
+  description = "DEV IP Address"
+  value       = proxmox_vm_qemu.server.default_ipv4_address
+}
